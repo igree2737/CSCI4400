3db6e5632bfd1494feeac5514c5286537d83e538
igree2737 <31322523+igree2737@users.noreply.github.com>
2018-04-13 11:32:11 -0400

    Update FrontDesk.java

--- RIS4400/src/FrontDesk/FrontDesk.java
+++ RIS4400/src/FrontDesk/FrontDesk.java
@@ -2,13 +2,15 @@ package FrontDesk;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -16,26 +18,18 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.stage.Stage;
-import javafx.util.StringConverter;
-import javafx.util.converter.DateTimeStringConverter;
 import javafx.util.converter.IntegerStringConverter;
-
 import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
-import java.time.temporal.TemporalAccessor;
-
 import javax.swing.text.MaskFormatter;
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.Statement;
-import javafx.scene.control.TableColumn.CellEditEvent;
-import javafx.scene.control.TablePosition;
 
 public class FrontDesk {
 	
@@ -44,18 +38,20 @@ public class FrontDesk {
 	
 	java.util.Date date;
 	java.sql.Date sqlDate;
+	
 	DateFormat formatter = new SimpleDateFormat("mm/dd/yyyy");
 	DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-	String pattern = "dd/MM/yyyy";
 	
 	Parent root;
 	Stage stage;
-	ObservableList<patientData>patientRecord;
-	ObservableList<patientData>emptyChecker;
+	
+	ObservableList<patientData>patientRecord = FXCollections.observableArrayList();
+	ObservableList<String>options;
 	
 	@FXML Button PatientButton;
 	@FXML Button ConfirmPatientButton;
 	@FXML Button ScheduleButton;
+	@FXML Button searchButton;
 	@FXML TextField FirstNameField;
 	@FXML TextField LastNameField;
 	@FXML TextField	IDfield;
@@ -67,6 +63,14 @@ public class FrontDesk {
 	@FXML TextField AddrField1;
 	@FXML TextField AddrField2;
 	@FXML TextField ZIPfield;
+	@FXML TextField fnameSearch;
+	@FXML TextField	lnameSearch;
+	@FXML TextField bdaySearch;
+	@FXML TextField	phoneSearch;
+	@FXML TextField citySearch;
+	@FXML TextField stateSearch;
+	@FXML TextField	zipSearch;
+	@FXML TextField searchText;
 	@FXML TableView<patientData> archiveTable;
 	@FXML TableColumn<patientData,Integer> idcol;
 	@FXML TableColumn<patientData,String> firstNameCol;
@@ -79,11 +83,11 @@ public class FrontDesk {
 	@FXML TableColumn<patientData,String> stateCol;
 	@FXML TableColumn <patientData,Integer>zipCol;
 	@FXML Tab archiveTab;
+	@FXML ChoiceBox<String> choiceBox;
 	
 	//opens patient form in a new window
 	public void patientwindow(ActionEvent event)throws Exception
 	{
-
 		try {
 			System.out.println("New Patient");
 			root = FXMLLoader.load(getClass().getResource("/FrontDeskView/FrontDeskNewPatient.fxml"));
@@ -112,6 +116,8 @@ public class FrontDesk {
 			e.printStackTrace();
 		}
 		
+		//getting text entered into textfields
+		//if nothing was entered, sets a default value
 		String fname = FirstNameField.getText();
 		if (fname.trim().equals(""))
 			fname = "NONE";
@@ -119,8 +125,12 @@ public class FrontDesk {
 		if (lname.trim().equals(""))
 			lname = "NONE";
 		String DOB = DOBfield.getText();
-		if (DOB.trim().equals(""))
-			DOB = "NONE";
+		if (DOB.trim().equals("")||DOB.trim().equals(null))
+		{
+			DOB = "00/00/0000";
+			date = formatter.parse(DOB);
+			sqlDate = new java.sql.Date(date.getTime());
+		}
 		else
 		{
 			date = formatter.parse(DOB);
@@ -149,17 +159,12 @@ public class FrontDesk {
 		if(!ZIPfield.getText().isEmpty())
 			Zip = Integer.parseInt(ZIPfield.getText());
 		
-			
+		//formats phone numbers	
 		String phoneMask= "###-###-####";
-		
-		//converts string to date
-		//date = formatter.parse(DOB);
-		//sqlDate = new java.sql.Date(date.getTime());
-
 		MaskFormatter format = new MaskFormatter(phoneMask);
 		format.setValueContainsLiteralCharacters(false);
 		Num = format.valueToString(Num);
-			
+		//	
 			
 			//inserts data into patient table
 			try {
@@ -190,12 +195,11 @@ public class FrontDesk {
 					System.out.println("Row not inserted.");
 				}
 				
-				
-			
-			} catch (Exception e) {
+				} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			
 			finally{
 				try {
 					if (smt != null)
@@ -216,141 +220,120 @@ public class FrontDesk {
 	//loads patient record from DB to the archive table
 	public void loadFromDB()
 	{
-		patientRecord = FXCollections.observableArrayList();
+		//initializes the choicebox for the archive table
+		options = FXCollections.observableArrayList("First Name","Last Name","Birth","Phone","City","State","Zip");
+		choiceBox.setItems(options);
+		choiceBox.setValue("First Name");
+		//
 		
 		idcol.setCellValueFactory(new PropertyValueFactory<patientData,Integer>("Id"));
 		
 		firstNameCol.setCellValueFactory(new PropertyValueFactory<patientData,String>("firstName"));
 		firstNameCol.setCellFactory(TextFieldTableCell.<patientData>forTableColumn());
-        firstNameCol.setOnEditCommit(
-                new EventHandler<CellEditEvent<patientData, String>>() {
-                    @Override
-                    public void handle(CellEditEvent<patientData, String> t) {
-                        ((patientData) t.getTableView().getItems().get(
-                                t.getTablePosition().getRow())
-                                ).setFirstName(t.getNewValue());
-                    }
-                }
-                );
+        firstNameCol.setOnEditCommit(event -> {
+        	patientData patient = event.getRowValue();
+        	patient.setFirstName(event.getNewValue());
+        	try {
+				updateDB("fName",event.getNewValue(),patient.getId());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        });
 		lastNameCol.setCellValueFactory(new PropertyValueFactory<patientData,String>("lastName"));
-		lastNameCol.setEditable(true);
 		lastNameCol.setCellFactory(TextFieldTableCell.<patientData>forTableColumn());
-        lastNameCol.setOnEditCommit(
-                new EventHandler<CellEditEvent<patientData, String>>() {
-                    @Override
-                    public void handle(CellEditEvent<patientData, String> t) {
-                        ((patientData) t.getTableView().getItems().get(
-                                t.getTablePosition().getRow())
-                                ).setLastName(t.getNewValue());
-                    }
-                }
-                );
-		dobcol.setCellValueFactory(new PropertyValueFactory<patientData,Date>("Bdate"));
-		dobcol.setCellFactory(TextFieldTableCell.<patientData, Date>forTableColumn(new StringConverter<Date>(){
-			@Override
-			public String toString(Date value){
-				return (value);
+        lastNameCol.setOnEditCommit(event -> {
+        	patientData patient = event.getRowValue();
+        	patient.setLastName(event.getNewValue());
+        	try {
+				updateDB("lName",event.getNewValue(),patient.getId());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			@Override
-			public Date fromString(String string){
-				return null;
-			}
-		}));
-
-		phoneCol.setCellValueFactory(new PropertyValueFactory<patientData,String>("num"));
-		if(phoneCol.getText()=="" || phoneCol.getText()==null)
-		{
-			phoneCol.setCellFactory(TextFieldTableCell.<patientData>forTableColumn());
-			phoneCol.setOnEditCommit(
-					new EventHandler<CellEditEvent<patientData, String>>() {
-						@Override
-						public void handle(CellEditEvent<patientData, String> t) {
-							((patientData) t.getTableView().getItems().get(
-									t.getTablePosition().getRow())
-									).setNum(t.getNewValue());
-						}
-					}
-					);
-		};
-		addrCol.setCellValueFactory(new PropertyValueFactory<patientData,String>("address"));
-		if(addrCol.getText()==""||addrCol.getText()==null)
-		{
-			addrCol.setCellFactory(TextFieldTableCell.<patientData>forTableColumn());
-			addrCol.setOnEditCommit(
-					new EventHandler<CellEditEvent<patientData, String>>() {
-						@Override
-						public void handle(CellEditEvent<patientData, String> t) {
-							((patientData) t.getTableView().getItems().get(
-									t.getTablePosition().getRow())
-									).setAddress1(t.getNewValue());
-						}
-					}
-					);
-		};
-		addrCol2.setCellFactory(TextFieldTableCell.<patientData>forTableColumn());
-        if(addrCol2.getText()==""||addrCol2.getText()==null)
-        {
-        	addrCol2.setOnEditCommit(
-        			new EventHandler<CellEditEvent<patientData, String>>() {
-        				@Override
-        				public void handle(CellEditEvent<patientData, String> t) {
-        					((patientData) t.getTableView().getItems().get(
-        							t.getTablePosition().getRow())
-        							).setAddress2(t.getNewValue());
-        				}
-        			}
-        			);
-        };
-		cityCol.setCellValueFactory(new PropertyValueFactory<patientData,String>("city"));
-		TablePosition pos = archiveTable.getSelectionModel().getSelectedCells().get(0);
-		int row = pos.getRow();
-		patientData item = archiveTable.getItems().get(row);
-		TableColumn col = pos.getTableColumn();
-		String data = (String) col.getCellObservableValue(item).getValue();
-		if(cityCol.getTableView().getItems())==""||cityCol.getText()==null)
-		{
-			cityCol.setCellFactory(TextFieldTableCell.<patientData>forTableColumn());
-			cityCol.setOnEditCommit(
-					new EventHandler<CellEditEvent<patientData, String>>() {
-						@Override
-	                    public void handle(CellEditEvent<patientData, String> t) {
-	                        ((patientData) t.getTableView().getItems().get(
-	                                t.getTablePosition().getRow())
-	                                ).setCity(t.getNewValue());
-	                    }
-	                }
-	                );
-		};
-		stateCol.setCellValueFactory(new PropertyValueFactory<patientData,String>("state"));
-		if(stateCol.getText()==""||stateCol.getText()==null)
-		{
-			stateCol.setCellFactory(TextFieldTableCell.<patientData>forTableColumn());
-	        stateCol.setOnEditCommit(
-	                new EventHandler<CellEditEvent<patientData, String>>() {
-	                    @Override
-	                    public void handle(CellEditEvent<patientData, String> t) {
-	                        ((patientData) t.getTableView().getItems().get(
-	                                t.getTablePosition().getRow())
-	                                ).setState(t.getNewValue());
-	                    }
-	                }
-	                );
-		};
-		zipCol.setCellValueFactory(new PropertyValueFactory<patientData,Integer>("zip"));
-		if(zipCol.)
-		zipCol.setCellFactory(TextFieldTableCell.<patientData,Integer>forTableColumn(new IntegerStringConverter()));
-        zipCol.setOnEditCommit(
-                new EventHandler<CellEditEvent<patientData, Integer>>() {
-                    @Override
-                    public void handle(CellEditEvent<patientData, Integer> t) {
-                        ((patientData) t.getTableView().getItems().get(
-                                t.getTablePosition().getRow())
-                                ).setZip(t.getNewValue());
-                    }
-                }
-                );
+        });
 		
-		try 
+        dobcol.setCellValueFactory(new PropertyValueFactory<patientData,Date>("Bdate"));
+        //TO DO: Find a way to update Date type objects
+        
+		phoneCol.setCellValueFactory(new PropertyValueFactory<patientData,String>("num"));
+		phoneCol.setCellFactory(TextFieldTableCell.<patientData>forTableColumn());
+		phoneCol.setOnEditCommit(event -> {
+        	patientData patient = event.getRowValue();
+        	patient.setNum(event.getNewValue());
+        	try {
+				updateDB("phoneNum",event.getNewValue(),patient.getId());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        });
+		
+		addrCol.setCellValueFactory(new PropertyValueFactory<patientData,String>("address1"));
+		addrCol.setCellFactory(TextFieldTableCell.<patientData>forTableColumn());
+		addrCol.setOnEditCommit(event -> {
+        	patientData patient = event.getRowValue();
+        	patient.setAddress1(event.getNewValue());
+        	try {
+				updateDB("addressOne",event.getNewValue(),patient.getId());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        });
+		addrCol2.setCellValueFactory(new PropertyValueFactory<patientData,String>("address2"));
+		addrCol2.setCellFactory(TextFieldTableCell.<patientData>forTableColumn());
+        addrCol2.setOnEditCommit(event -> {
+        	patientData patient = event.getRowValue();
+        	patient.setAddress2(event.getNewValue());
+        	try {
+				updateDB("addressTwo",event.getNewValue(),patient.getId());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        });
+        
+		cityCol.setCellValueFactory(new PropertyValueFactory<patientData,String>("city"));
+		cityCol.setCellFactory(TextFieldTableCell.<patientData>forTableColumn());
+		cityCol.setOnEditCommit(event -> {
+        	patientData patient = event.getRowValue();
+        	patient.setCity(event.getNewValue());
+        	try {
+				updateDB("addressCity",event.getNewValue(),patient.getId());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        });
+		
+		stateCol.setCellValueFactory(new PropertyValueFactory<patientData,String>("state"));
+		stateCol.setCellFactory(TextFieldTableCell.<patientData>forTableColumn());
+	    stateCol.setOnEditCommit(event -> {
+        	patientData patient = event.getRowValue();
+        	patient.setState(event.getNewValue());
+        	try {
+				updateDB("addressState",event.getNewValue(),patient.getId());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        });
+		
+		zipCol.setCellValueFactory(new PropertyValueFactory<patientData,Integer>("zip"));
+		zipCol.setCellFactory(TextFieldTableCell.<patientData,Integer>forTableColumn(new IntegerStringConverter()));
+        zipCol.setOnEditCommit(event -> {
+        	patientData patient = event.getRowValue();
+        	patient.setZip(event.getNewValue());
+        	try {
+				updateDB("addressZip",event.getNewValue().toString(),patient.getId());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        });
+                
+        try 
 		{
 			Class.forName("com.mysql.jdbc.Driver");  
 			con=(Connection) DriverManager.getConnection(  
@@ -370,16 +353,69 @@ public class FrontDesk {
 			while(rs.next())
 			{
 				patientRecord.add(new patientData(rs.getInt("patientID"),rs.getString("fName"),rs.getString("lName"),rs.getDate("dateOfBirth"),rs.getString("phoneNum"),
-						rs.getString("addressOne"),rs.getString("addressCity"),rs.getString("addressState"),rs.getInt("addressZip")));
+						rs.getString("addressOne"),rs.getString("addressTwo"),rs.getString("addressCity"),rs.getString("addressState"),rs.getInt("addressZip")));
 			}
 			
 		}
+		//
 		catch(Exception e)
 		{
 			e.printStackTrace();
 		}
 		
-		archiveTable.setItems(patientRecord);
+		archiveSearch();
+	}
+	
+	//Updates the database to reflect edited values
+	public void updateDB(String column, String newValue, int id) throws SQLException
+	{
+		try 
+		{
+			Class.forName("com.mysql.jdbc.Driver");  
+			con=(Connection) DriverManager.getConnection(  
+			"jdbc:mysql://localhost:3306/RISystem","root","admin123"); //change to whatever your username/password is 
+			smt=(Statement) con.createStatement(); 
+		}
+		
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		PreparedStatement stmt = con.prepareStatement("UPDATE patient SET "+column+" = ? WHERE patientID = ? ");
+		stmt.setLong(1, Integer.parseInt(newValue));
+        stmt.setInt(2, id);
+        stmt.execute();
+	}
+	
+	public void archiveSearch()
+	{
+		FilteredList<patientData> filteredData = new FilteredList<>(patientRecord, p -> true);
+		searchText.textProperty().addListener((observable, oldValue, newValue) -> {
+	            filteredData.setPredicate(p -> {
+	                // If filter text is empty, display all persons.
+	                if (newValue == null || newValue.isEmpty()) {
+	                    return true;
+	                }
+
+	                // Compare first name and last name of every person with filter text.
+	                String lowerCaseFilter = newValue.toLowerCase();
+
+	                if (p.getFirstName().toLowerCase().contains(lowerCaseFilter)) {
+	                    return true; // Filter matches first name.
+	                } else if (p.getLastName().toLowerCase().contains(lowerCaseFilter)) {
+	                    return true; // Filter matches last name.
+	                }
+	                return false; // Does not match.
+	            });
+	        });
+		SortedList<patientData> sortedData = new SortedList<>(filteredData);
+
+        // 4. Bind the SortedList comparator to the TableView comparator.
+        sortedData.comparatorProperty().bind(archiveTable.comparatorProperty());
+
+        // 5. Add sorted (and filtered) data to the table.
+        archiveTable.setItems(sortedData);
 	}
 	
 }
